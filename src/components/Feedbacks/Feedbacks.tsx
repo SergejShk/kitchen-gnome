e3589e501033de7e63f5955b66a76496457acceb
SergejShk <sshkurupij11@gmail.com>
2023-12-26 14:59:47 +0200

    Feedbacks/ Added mouseWheel for swiper

--- src/components/Feedbacks/Feedbacks.tsx
+++ src/components/Feedbacks/Feedbacks.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Controller, Keyboard, Mousewheel, Pagination } from "swiper/modules";
 
 import { feedbacks } from "../../utils/constants";
 
@@ -9,6 +10,8 @@ import "swiper/css/pagination";
 import s from "./feedbacks.module.css";
 
 const Feedbacks = () => {
+	const [imgSwiper, setImgSwiper] = useState<null | any>(null);
+
 	return (
 		<section className={s.feedbacksSection} id="feedbacks">
 			<div className={s.feedbacksContainer}>
@@ -16,15 +19,39 @@ const Feedbacks = () => {
 				<h2 className={s.feedbacksTitle}>Feedbacks</h2>
 
 				<Swiper
+					modules={[Controller, Pagination, Keyboard, Mousewheel]}
+					controller={{ control: imgSwiper }}
 					grabCursor
-					modules={[Pagination]}
+					className={`${s.swiperHidden}`}
 					pagination={{
 						el: ".feedbacks-pagination",
 						clickable: true,
 					}}
+					keyboard={{
+						enabled: true,
+						onlyInViewport: true,
+						pageUpDown: true,
+					}}
+					mousewheel={{
+						sensitivity: 1,
+						forceToAxis: false,
+						releaseOnEdges: true,
+					}}
 					breakpoints={{
-						375: { slidesPerView: 1.1 },
-						1440: { slidesPerView: 3.65 },
+						375: { touchRatio: 2, slidesPerGroup: 1 },
+						1440: { touchRatio: 10, slidesPerGroup: 3.5 },
+					}}
+				>
+					{feedbacks.map((_, index) => (
+						<SwiperSlide key={index}></SwiperSlide>
+					))}
+				</Swiper>
+
+				<Swiper
+					onSwiper={setImgSwiper}
+					breakpoints={{
+						375: { slidesPerView: 1.1, slidesPerGroup: 1 },
+						1440: { slidesPerView: 3.65, slidesPerGroup: 3.5 },
 					}}
 				>
 					{feedbacks.map((feedback, index) => (
